f0be6381d784a63340bb1ee945a47998fb8aaf57
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-06-08 15:57:15 +0700

    fix img

--- src/components/atoms/BackgroundHeader.jsx
+++ src/components/atoms/BackgroundHeader.jsx
@@ -2,7 +2,7 @@ function BackgroundHeader({ children }) {
   return (
     <>
       <div className="bg-gradient-to-b from-[#000000] to-[#181A1C]">
-        <div class="bg-[linear-gradient(to_top,rgba(0,0,0,0.9)_0%,rgba(0,0,0,0)_60%),url('images/movies/bg-duty.png')] bg-cover bg-center overflow-hidden flex flex-col min-h-60 pl-7 pt-20 pb-10 pr-4 lg:pl-20 lg:pt-40 lg:pr-20">
+        <div class="bg-[linear-gradient(to_top,rgba(0,0,0,0.9)_0%,rgba(0,0,0,0)_60%),url('/images/movies/bg-duty.png')] bg-cover bg-center overflow-hidden flex flex-col min-h-60 pl-7 pt-20 pb-10 pr-4 lg:pl-20 lg:pt-40 lg:pr-20">
           {children}
         </div>
       </div>
--- src/components/atoms/FilmContentThird.jsx
+++ src/components/atoms/FilmContentThird.jsx
@@ -7,37 +7,37 @@ const FilmContentThird = forwardRef((props, ref) => {
         id="trending"
         class="mt-5 flex flex-nowrap overflow-x-auto gap-4 scroll-smooth snap-center lg:min-w-full lg:gap-7 lg:overflow-x-hidden"
       >
-        <div class="bg-[url('images/movies/thetomorrow.png')] min-w-24 h-36 bg-cover bg-center rounded flex relative lg:min-w-[234px] lg:min-h-[365px]">
+        <div class="bg-[url('/images/movies/thetomorrow.png')] min-w-24 h-36 bg-cover bg-center rounded flex relative lg:min-w-[234px] lg:min-h-[365px]">
           <div class="flex flex-wrap p-[2px] w-[14px] h-[21px] bg-red-500 top-0 right-1 absolute">
             <span class="font-normal text-[6px] leading-[140%]">Top</span>
             <span class="font-normal text-[6px] leading-[140%]">10</span>
           </div>
         </div>
-        <div class="bg-[url('images/movies/quantumania.png')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
+        <div class="bg-[url('/images/movies/quantumania.png')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
           <div class="flex flex-wrap p-[2px] w-[14px] h-[21px] bg-red-500 top-0 right-1 absolute">
             <span class="font-normal text-[6px] leading-[140%]">Top</span>
             <span class="font-normal text-[6px] leading-[140%]">10</span>
           </div>
         </div>
-        <div class="bg-[url('images/movies/littlemermaid.png')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
+        <div class="bg-[url('/images/movies/littlemermaid.png')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
           <div class="flex flex-wrap p-[2px] w-[14px] h-[21px] bg-red-500 top-0 right-1 absolute">
             <span class="font-normal text-[6px] leading-[140%]">Top</span>
             <span class="font-normal text-[6px] leading-[140%]">10</span>
           </div>
         </div>
-        <div class="bg-[url('images/movies/infinitywar.jpg')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
+        <div class="bg-[url('/images/movies/infinitywar.jpg')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
           <div class="flex flex-wrap p-[2px] w-[14px] h-[21px] bg-red-500 top-0 right-1 absolute">
             <span class="font-normal text-[6px] leading-[140%]">Top</span>
             <span class="font-normal text-[6px] leading-[140%]">10</span>
           </div>
         </div>
-        <div class="bg-[url('images/movies/ghibli.jpg')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
+        <div class="bg-[url('/images/movies/ghibli.jpg')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
           <div class="flex flex-wrap p-[2px] w-[14px] h-[21px] bg-red-500 top-0 right-1 absolute">
             <span class="font-normal text-[6px] leading-[140%]">Top</span>
             <span class="font-normal text-[6px] leading-[140%]">10</span>
           </div>
         </div>
-        <div class="bg-[url('images/movies/fastandfurious.jpg')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
+        <div class="bg-[url('/images/movies/fastandfurious.jpg')] min-w-24 h-36 bg-cover bg-center rounded relative lg:min-w-[234px] lg:min-h-[365px]">
           <div class="flex flex-wrap p-[2px] w-[14px] h-[21px] bg-red-500 top-0 right-1 absolute">
             <span class="font-normal text-[6px] leading-[140%]">Top</span>
             <span class="font-normal text-[6px] leading-[140%]">10</span>
