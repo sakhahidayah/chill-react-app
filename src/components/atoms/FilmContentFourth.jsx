ffdfd68e4145b994715720b30be603c0cce39548
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-06-10 07:29:48 +0700

    update episode baru icon

--- src/components/atoms/FilmContentFourth.jsx
+++ src/components/atoms/FilmContentFourth.jsx
@@ -9,7 +9,7 @@ const FilmContentFourth = forwardRef((props,ref) => {
         className="mt-5 flex flex-nowrap overflow-x-auto gap-4 scroll-smooth snap-center lg:min-w-full lg:gap-7 lg:overflow-x-hidden"
       >
         <div className="bg-[url('/images/movies/dutyafter.png')] min-w-24 h-36 bg-cover bg-center rounded pt-2 pl-2 lg:min-w-[234px] lg:min-h-[365px]">
-          <p className="bg-[#0F1E93] rounded-xl w-[44px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[5.74px] leading-[140%] border-[#0F1E93]">
+          <p className="bg-[#0F1E93] rounded-xl w-[55px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[6.74px] leading-[140%] border-[#0F1E93]">
             Episode baru
           </p>
         </div>
@@ -32,7 +32,7 @@ const FilmContentFourth = forwardRef((props,ref) => {
           </div>
         </div>
         <div className="bg-[url('/images/movies/squidgame.jpg')] min-w-24 h-36 bg-cover bg-center rounded pt-2 pl-2 lg:min-w-[234px] lg:min-h-[365px]">
-          <p className="bg-[#0F1E93] rounded-xl w-[44px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[5.74px] leading-[140%] border-[#0F1E93]">
+          <p className="bg-[#0F1E93] rounded-xl w-[55px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[6.74px] leading-[140%] border-[#0F1E93]">
             Episode baru
           </p>
         </div>
--- src/components/atoms/FilmContentSecond.jsx
+++ src/components/atoms/FilmContentSecond.jsx
@@ -8,19 +8,19 @@ const FilmContentSecond = forwardRef((props,ref) => {
         class="mt-5 flex flex-nowrap overflow-x-auto gap-4 scroll-smooth snap-center snap-x lg:min-w-full lg:gap-7 lg:overflow-x-hidden"
       >
         <div class="bg-[url('/images/movies/suzume.png')] min-w-24 h-36 bg-cover bg-center rounded pt-2 pl-2 lg:min-w-[234px] lg:min-h-[365px] snap-end">
-          <p class="bg-[#0F1E93] rounded-xl w-[44px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[5.74px] leading-[140%] border-[#0F1E93]">
+           <p className="bg-[#0F1E93] rounded-xl w-[55px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[6.74px] leading-[140%] border-[#0F1E93]">
             Episode baru
           </p>
         </div>
         <div class="bg-[url('/images/movies/sonic.png')] min-w-24 h-36 bg-cover bg-center rounded lg:min-w-[234px] lg:min-h-[365px] snap-end"></div>
         <div class="bg-[url('/images/movies/big-hero.png')] min-w-24 h-36 bg-cover bg-center rounded pt-2 pl-2 lg:min-w-[234px] lg:min-h-[365px] snap-end">
-          <p class="bg-[#0F1E93] rounded-xl w-[44px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[5.74px] leading-[140%] border-[#0F1E93]">
+          <p className="bg-[#0F1E93] rounded-xl w-[55px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[6.74px] leading-[140%] border-[#0F1E93]">
             Episode baru
           </p>
         </div>
         <div class="bg-[url('/images/movies/jurasicworld.png')] min-w-24 h-36 bg-cover bg-center rounded lg:min-w-[234px] lg:min-h-[365px] snap-end"></div>
         <div class="bg-[url('/images/movies/allofus.png')] min-w-24 h-36 bg-cover bg-center rounded pt-2 pl-2 lg:min-w-[234px] lg:min-h-[365px] snap-end">
-          <p class="bg-[#0F1E93] rounded-xl w-[44px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[5.74px] leading-[140%] border-[#0F1E93]">
+        <p className="bg-[#0F1E93] rounded-xl w-[55px] h-[14px] px-1 py-0.5 border-[0.5px] font-bold text-[6.74px] leading-[140%] border-[#0F1E93]">
             Episode baru
           </p>
         </div>
