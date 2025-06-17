97893c94b10de8251f77d1c9a90af14b8f4c0d30
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-06-17 22:30:26 +0700

    updated button slide

--- src/components/atoms/BtnSlide.jsx
+++ src/components/atoms/BtnSlide.jsx
@@ -11,15 +11,15 @@ function BtnSlide({ containerRef }) {
     <>
       <button
         onClick={handleScrollLeft}
-        className="absolute z-10 top-1/2 -left-0 lg:left-[70px]"
+        className="absolute z-10 top-1/2 -left-0 lg:left-[70px] "
       >
-        <i className="fas fa-arrow-left bg-[#2F3334] rounded-3xl p-3"></i>
+        <i className="fas fa-arrow-left bg-[#2F3334] rounded-3xl p-3 hover:bg-slate-500 cursor-pointer"></i>
       </button>
       <button
         onClick={handleScrollRight}
         className="absolute z-10 right-0 top-1/2"
       >
-        <i className="fas fa-arrow-right bg-[#2F3334] rounded-3xl p-3"></i>
+        <i className="fas fa-arrow-right bg-[#2F3334] rounded-3xl p-3 hover:bg-slate-500 cursor-pointer"></i>
       </button>
     </>
   );
