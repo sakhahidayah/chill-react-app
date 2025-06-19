56250f3632102f2a35270132947196fecc4b5b31
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-06-20 06:23:32 +0700

    menyesuaikan text di header

--- src/components/atoms/Caption.jsx
+++ src/components/atoms/Caption.jsx
@@ -2,11 +2,11 @@ function Caption() {
   return (
     <>
      
-      <div className="lg:w-[500px] lg:mb-10">
-        <h4 className="font-bold text-2xl leading-[120%] mb-3">
+      <div className="lg:w-[500px] lg:mb-10 md:w-[100%]">
+        <h4 className="font-bold text-2xl md:text-4xl leading-[120%] mb-3">
           Duty After School
         </h4>
-        <p className="font-medium text-xs leading-[140%] tracking-wide mb-3">
+        <p className="font-medium text-xs md:text-xl  leading-[140%] tracking-wide mb-3">
           Sebuah benda tak dikenal mengambil alih dunia. Dalam keputusasaan,
           Departemen Pertahanan mulai merekrut lebih banyak tentara, termasuk
           siswa sekolah menengah. Mereka pun segera menjadi pejuang garis depan
--- src/components/organism/Home/DaftarContent.jsx
+++ src/components/organism/Home/DaftarContent.jsx
@@ -1,7 +1,7 @@
 function DaftarContent() {
   return (
     <>
-      <div className="flex flex-col  items-center min-h-screen pl-20 pt-5">
+      <div className="flex flex-col w-screen h-screen items-center justify-center pl-20 pt-5">
         <h1 className="text-2xl font-bold my-3">Daftar Film</h1>
         <p>Belum ada film yang disimpan</p>
       </div>
